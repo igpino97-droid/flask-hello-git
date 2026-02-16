b83e067afcbece67a581670769405244739df1d4
Ignacio Pino <igpino97@gmail.com>
2026-02-16 00:59:41 +0000

    feat: initial k8s deployment with ingress

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World probando probando"
+    return "Hello World desde Flask y Git 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
