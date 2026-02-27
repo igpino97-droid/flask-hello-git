ebf2721414bcad8725b150dcc35021e4365b2587
Ignacio Pino <igpino97@gmail.com>
2026-02-27 02:42:43 +0000

    feat: version 2 staging

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World desde Flask y Git 🚀"
+    return "PROBANDO LA APP DEL NACHO"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
