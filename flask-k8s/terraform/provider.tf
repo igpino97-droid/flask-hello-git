3ab499a13006c9c285881cec664e1d8a8fb4e81c
Ignacio Pino <igpino97@gmail.com>
2026-03-15 22:45:19 +0000

    test CI/CD

---
+++ flask-k8s/terraform/provider.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project = "flask-k8s-cicd-489601"
+  region  = "us-central1"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
